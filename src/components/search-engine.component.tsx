760a3bf51a7205130b238cc1520e239204c71ef9
asgorlov <asgorlov@outlook.com>
2022-07-30 14:10:41 +0300

    Changed styles & searched engine component

--- src/components/search-engine.component.tsx
+++ src/components/search-engine.component.tsx
@@ -10,23 +10,19 @@ interface SearchEngineProps {
 }
 
 const SearchEngineComponent: FC<SearchEngineProps> = ({
-                                                          searchEngine,
-                                                          localization,
-                                                          logo
+    searchEngine,
+    localization,
+    logo
 }) => {
-    const searchEngineLink = SEARCH_ENGINE_LINKS[searchEngine];
-    const searchQueryLink = SEARCH_QUERY_LINKS[searchEngine];
-    const searchQueryPlaceholder = LOCALIZED_SEARCH_QUERY[localization];
-    const searchButtonInnerText = LOCALIZED_SEARCH_BUTTON[localization];
-
     return (<div className={`${searchEngine}_search_engine`}>
-            <a href={searchEngineLink}>
+            <a href={SEARCH_ENGINE_LINKS[searchEngine]}>
                 <div className={`${searchEngine}_${logo}`}></div>
             </a>
-            <form className={`${searchEngine}_search_form`} action={searchQueryLink}>
+            <form className={`${searchEngine}_search_form`}
+                  action={SEARCH_QUERY_LINKS[searchEngine]}>
                 <div className={`${searchEngine}_search_input`}>
                     <input className={`${searchEngine}_search_input_control`}
-                           placeholder={searchQueryPlaceholder}
+                           placeholder={LOCALIZED_SEARCH_QUERY[localization]}
                            tabIndex={2}
                            autoComplete="off"
                            maxLength={400}
@@ -36,7 +32,7 @@ const SearchEngineComponent: FC<SearchEngineProps> = ({
                     <button className={`${searchEngine}_search_button_theme`}
                             tabIndex={1} type="submit">
                         <span className={`${searchEngine}_search_button_text`}>
-                            {searchButtonInnerText}
+                            {LOCALIZED_SEARCH_BUTTON[localization]}
                         </span>
                     </button>
                 </div>
