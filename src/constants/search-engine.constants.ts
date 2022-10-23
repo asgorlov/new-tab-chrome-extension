14bd3f0c6c5b7b665a7947806678045db433c561
asgorlov <asgorlov@outlook.com>
2022-10-23 22:16:27 +0600

    fixed link engine bug and remove unnecessary files

--- src/constants/search-engine.constants.ts
+++ src/constants/search-engine.constants.ts
@@ -3,13 +3,13 @@ export const GOOGLE = "google";
 export const DUCK = "duckduckgo";
 
 export const SEARCH_ENGINE_LINKS: Record<string,string> = {
-    YANDEX: "https://ya.ru/",
-    GOOGLE: "https://google.com/",
-    DUCK: "https://duckduckgo.com/",
+    "yandex": "https://ya.ru/",
+    "google": "https://google.com/",
+    "duckduckgo": "https://duckduckgo.com/",
 };
 
 export const SEARCH_QUERY_LINKS: Record<string,string> = {
-    YANDEX: "https://yandex.ru/search/",
-    GOOGLE: "https://google.com/search",
-    DUCK: "https://duckduckgo.com",
+    "yandex": "https://yandex.ru/search/",
+    "google": "https://google.com/search",
+    "duckduckgo": "https://duckduckgo.com",
 };
