c0db7006655042e1dc9106ae1a582aad5a328d8f
GorlovAS <gorlovas@rtk-cd.ru>
2023-05-02 18:21:50 +0300

    Разбита локализация по файлам конкретного языка

--- src/localizations/i18n.ts
+++ src/localizations/i18n.ts
@@ -1,35 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import ru_RU from "./translations/ru-RU.json";
+import en_US from "./translations/en-US.json";
 
 i18n.use(initReactI18next).init({
-  resources: {
-    ru: {
-      translation: {
-        webSearch: "Поиск в Интернете",
-        tabTitle: "Новая вкладка",
-        searchQuery: "Введите поисковый запрос...",
-        searchButton: "Найти",
-        darkModeTitle: "Темный режим",
-        turnOn: "Вкл.",
-        turnOff: "Выкл.",
-        auto: "Авто",
-        manual: "Вручную"
-      }
-    },
-    en: {
-      translation: {
-        webSearch: "Web search",
-        tabTitle: "New tab",
-        searchQuery: "Enter search query...",
-        searchButton: "Search",
-        darkModeTitle: "Dark mode",
-        turnOn: "On",
-        turnOff: "Off",
-        auto: "Auto",
-        manual: "Manual"
-      }
-    }
-  },
+  resources: { ...ru_RU, ...en_US },
   lng: window.navigator.language ? window.navigator.language : "ru-RU",
   fallbackLng: "en-EN"
 });
