774540e484f986e067e31058db71018d1cef11a7
GorlovAS <gorlovas@rtk-cd.ru>
2023-10-02 17:30:00 +0300

    [NTCE-2] Удалить js скрипт из паблик папки

--- public/init.js
+++
@@ -1,6 +0,0 @@
-const initBackground = async () => {
-  const data = chrome.storage?.sync.get({ isDark: false });
-  document.body.style.backgroundColor = data?.isDark ? "#292c35" : "#fff";
-};
-
-await initBackground();
