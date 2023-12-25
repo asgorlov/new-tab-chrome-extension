ac45a5139a847206a76d44036d3985a0eb9d3b61
GorlovAS <gorlovas@rtk-cd.ru>
2023-12-25 16:56:59 +0300

    Добавлен await для получения флага темного режима

--- public/init.js
+++ public/init.js
@@ -1,6 +1,6 @@
 // Скорость загрузки на странице этого скрипта быстрее, чем через ts + webpack
 const initBackground = async () => {
-  const data = chrome.storage?.sync.get({ isDark: false });
+  const data = await chrome.storage?.sync.get({ isDark: false });
   document.body.style.backgroundColor = data?.isDark ? "#292c35" : "#fff";
 };
 
